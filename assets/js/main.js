a874ce49ce02e3b7ea46fa6894d373d88e8ab39b
ricardosilva1990 <33942740+ricardosilva1990@users.noreply.github.com>
2018-09-21 21:25:14 +0100

    Original Commit
    Submit the current version of the website.

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($) {
+
+
+    /*======= Skillset *=======*/
+    
+    
+    $('.level-bar-inner').css('width', '0');
+    
+    $(window).on('load', function() {
+
+        $('.level-bar-inner').each(function() {
+        
+            var itemWidth = $(this).data('level');
+            
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+            
+        });
+
+    });
+
+});
+
+$(function() {
+  $("input[id$='_btn']").change(function() {
+     document.documentElement.lang = $(this).data($(this).prop("checked").toString());
+  });
+});
